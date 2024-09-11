0cffff1330d1c06584361279de9dfbcaab7e9973
km.kim <develover23@gmail.com>
2024-09-11 15:38:08 +0900

    🚧 Detect position of tab-panels

--- js/product-tab.js
+++ js/product-tab.js
@@ -37,3 +37,40 @@ prooductTabButtonList.forEach(function (button) {
   button.addEventListener('click', toggleActiveTab)
   button.addEventListener('click', scrollToTabPanel)
 })
+
+// 1 . 각 tabPanel 의 y축 위치 (문서의 시작점에서부터 얼마나 아래에 있는지)
+// 2. 요소의 y 축 위치 = window.scrollY + element.getBoundingCiientRect().top
+
+const productTabPanelIdList = [
+  'product-spec',
+  'product-review',
+  'product-inquiry',
+  'product-shipment',
+  'product-recommendation',
+]
+
+const productTabpPanelList = productTabPanelIdList.map(function (panelId) {
+  const tabPanel = document.querySelector(`#${panelId}`)
+  return tabPanel
+})
+
+const productTabpPanelPositionMap = {}
+
+function detectTabPanelPosition() {
+  // 각각의 tabPanel의 y축 위치를 찾는다
+  // productTabpPanelPositionMap에 그 값을 업데이트
+  // ex
+  // {
+  //   'product-spec': 1000,
+  //   'product-review': 5000,
+  //   ...
+  // }
+  productTabpPanelList.forEach(function (panel) {
+    const id = panel.getAttribute('id')
+    const position = window.scrollY + panel.getBoundingClientRect().top
+    productTabpPanelPositionMap[id] = position
+  })
+}
+
+window.addEventListener('load', detectTabPanelPosition)
+window.addEventListener('resize', detectTabPanelPosition)
